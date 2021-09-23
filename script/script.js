c4b838d294b3e973db48886f7bf7c94da9ac7ded
Aida1905 <miss.wowhey@gmail.com>
2021-09-23 22:14:25 +0600

    initial

---
+++ script/script.js
@@ -0,0 +1,101 @@
+//const a = 15 /
+
+//if (a===10){
+    //console.log('Verno')
+//}
+//else {
+    //console.log('Neverno')
+//}
+
+
+//let a = 50 /
+//let b = 100
+
+//if (a > b) {
+   // console.log('a bolshe b')
+//}
+
+//else {
+    //console.log('a menshe b')
+//}
+
+
+//const a = -2 /
+// if (a > 0){
+//     console.log('positive')
+// } else if(a === 0){
+//     console.log('ravno')
+// } else {
+//     console.log('negative')
+// }
+//task4
+// const number = 46
+// if (number % 2 === 0){
+//     console.log('chetnoe')
+// } else{
+//     console.log('nechetnoe')
+// }
+
+
+   // task5
+// const a = 10;
+// const b = 2;
+//
+// if(a % b === 0){
+//     console.log('Chetnoe chislo')
+// } else {
+//     console.log('Nechetnoe chislo')
+// }
+
+ //task6
+//  const s = 20;
+// if (typeof s === 'boolean'){
+//     console.log('boolean')
+// }else if (typeof s === 'string'){
+//     console.log('string')
+// } else if (typeof s === 'number'){
+//     console.log('number')
+// }
+
+
+ //task7
+// const a = 5;
+// if (a > 1 && a < 9){
+//    console.log("Verno")
+// } else {
+// console.log("Neverno")
+// }
+
+
+//task8
+// const a = 30;
+//
+// if(a === 3 || a === 7){
+//     console.log(a + 7)
+// } else{
+//     console.log(a / 10)
+// }
+
+// task9
+
+// const a = 15;
+// const b = 20;
+// if (a <= 0 && b >= 5){
+//     console.log(a + b)
+// }else{
+//     console.log(a - b)
+// }
+
+//task10
+//const a = 8
+//const b = 9
+//if (a > 4 && a < 10 || b >= 7 && b < 17) {
+    //console.log('Verno')
+//}
+//else {
+   // console.log('Neverno')
+//}
+
+
+
+
